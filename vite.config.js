26fd8070be301739cfbd36e620cf7a1ff72ca1bd
lianmaymesi <lianmay.mesi@gmail.com>
2026-02-28 17:00:50 +0530

    feat: added all basic styles env

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
-import {
-    defineConfig
-} from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/css/backoffice.css",
+                "resources/js/backoffice.js",
+                "resources/css/auth.css",
+                "resources/js/auth.js",
+                "resources/css/landing.css",
+                "resources/js/landing.js",
+            ],
             refresh: true,
         }),
         tailwindcss(),
@@ -15,7 +22,7 @@ export default defineConfig({
     server: {
         cors: true,
         watch: {
-            ignored: ['**/storage/framework/views/**'],
+            ignored: ["**/storage/framework/views/**"],
         },
     },
 });
